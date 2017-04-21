55f44520adceb05e72da300079f12e931581f8ee
Nicole Santamaria <nicolersantamaria@gmail.com>
2017-04-21 09:10:48 -0700

    add additional spec to check if two words are also palindromes. update method to include test. third spec will pass

--- lib/anagrams.rb
+++ lib/anagrams.rb
@@ -3,7 +3,9 @@ class String
     word_array = self.downcase().split('')
     compare_array = word_check.downcase.split('')
 
-    if word_array.uniq.sort == compare_array.uniq.sort
+    if (word_array.uniq.sort == compare_array.uniq.sort) && (word_array.reverse! == compare_array)
+      'These two words are anagrams! They are also palindromes!'
+    elsif word_array.uniq.sort == compare_array.uniq.sort
       'These two words are anagrams!'
     else
       'These two words are not anagrams.'
--- spec/anagrams_spec.rb
+++ spec/anagrams_spec.rb
@@ -8,4 +8,7 @@ describe('String#anagrams') do
   it('will check if two words are anagrams regardless of case') do
     expect('Ruby'.anagrams('Bury')).to(eq('These two words are anagrams!'))
   end
+  it('will check if two words are also palindromes') do
+    expect('rat'.anagrams('tar')).to(eq('These two words are anagrams! They are also palindromes!'))
+  end
 end
