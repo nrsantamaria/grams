7e75c5d6ef9417788636daa47333e9f311947c98
Nicole Santamaria <nicolersantamaria@gmail.com>
2017-04-21 09:27:09 -0700

    add spec to determine if word is a word. test will not pass, no additional code to method included

--- spec/anagrams_spec.rb
+++ spec/anagrams_spec.rb
@@ -11,4 +11,7 @@ describe('String#anagrams') do
   it('will check if two words are also palindromes') do
     expect('rat'.anagrams('tar')).to(eq('These two words are anagrams! They are also palindromes!'))
   end
+  it('will check if word includes vowels') do
+    expect('yurb'.anagrams('ruby')).to(eq('That is not a word, try again.'))
+  end
 end
